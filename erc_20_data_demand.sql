8850a70a7648fb406c905800ff21abb87de3d0a9
Басаргина Валентина <valentinabasargina@yandex.ru>
2025-05-14 14:13:53 +0300

    Update erc_20_data_demand.sql

--- erc_20_data_demand.sql
+++ erc_20_data_demand.sql
@@ -1,6 +1,8 @@
+-- Детализация заявки
 truncate table nrpz.erc_${year}_data_demand;
 insert into nrpz.erc_${year}_data_demand
-select  srl.org_kgntv,
+select d.*,  erc.nmc_reqnum, erc.ck_first FROM 
+(select  srl.org_kgntv,
 		eo.orgtitle grbstitle,
         dok.full_name,
         dok.inn,
@@ -8,15 +10,15 @@ select  srl.org_kgntv,
 		srl.publishdate_reqnum,
         -- числитель 8, 7, 6 показателя, знаменатель 6,7,8 показателя
         max(sd_denominator.max_PUBL) max_PUBL_zn,
-		max(sd_denominator.min_PUBL) min_PUBL_zn,
 		max(sd_denominator.cnt_all) cnt_all_zn,
 		max(sd_denominator.cnt_adm) cnt_adm_zn,
         max(sd_denominator.title) title_protocol_zn,
 		max(sd_denominator.flag_protocol) flag_protocol_zn,
 		srl.flag_smp,
 		srl.sop_name_reqnum,
-		ais.lotuuid, ais.requestid
-    from (select REQNUM, max(publishdate_reqnum) publishdate_reqnum, sop_name_reqnum,joflag, sop_code_reqnum, flag_smp,
+		ais.lotuuid, 
+		ais.requestid
+from (select REQNUM, max(publishdate_reqnum) publishdate_reqnum, sop_name_reqnum,joflag, sop_code_reqnum, flag_smp,
 						case 
                              when joflag = 1 and org_kgntv_joflag in (1412, 592) then grbsid
                              when joflag = 1 and org_kgntv_joflag not in  (1412, 592) then org_kgntv_joflag 
@@ -34,14 +36,14 @@ select  srl.org_kgntv,
                              when joflag = 1 and org_kgntv_joflag not in  (1412, 592) then org_kgntv_joflag 
                         else org_kgntv end 
 				)srl
-     join nrpz.erc_${year}_demand sd on srl.REQNUM = sd.purchasenumber And sd.flag_protocol in ('1','2','3') and sd.prolonflag is null
+join nrpz.erc_${year}_demand sd on srl.REQNUM = sd.purchasenumber And sd.flag_protocol in ('1','2','3') and sd.prolonflag is null
      -- знаменатель (учитываюся минимальные) / числитель письмо от 06.03.2022
-     left join (select * from nrpz.erc_${year}_demand where (purchasenumber, flag_protocol) in (select purchasenumber,min(flag_protocol)flag_protocol1 from nrpz.erc_${year}_demand group by purchasenumber)) sd_denominator on srl.REQNUM = sd_denominator.purchasenumber And sd_denominator.flag_protocol in ('1','2','3') and sd_denominator.prolonflag is null
+left join (select * from nrpz.erc_${year}_demand where (purchasenumber, flag_protocol) in (select purchasenumber,min(flag_protocol)flag_protocol1 from nrpz.erc_${year}_demand group by purchasenumber)) sd_denominator on srl.REQNUM = sd_denominator.purchasenumber And sd_denominator.flag_protocol in ('1','2','3') and sd_denominator.prolonflag is null
      -- числитель (учитываюся итоговые)
-     join nrpz.erc_dwh_organization_kgntv dok on dok.id = srl.org_kgntv
-	 join nrpz.erc_dwh_organization_kgntv dokgrbs on dokgrbs.id = dok.parentid
-	 join nrpz.ERC_ORGANIZATION eo	on eo.spz = dokgrbs.spz
-     left join 
+join nrpz.erc_dwh_organization_kgntv dok on dok.id = srl.org_kgntv
+join nrpz.erc_dwh_organization_kgntv dokgrbs on dokgrbs.id = dok.parentid
+join nrpz.ERC_ORGANIZATION eo	on eo.spz = dokgrbs.spz
+left join 
 	(select reqnum, 
 		string_agg(lotuuid::varchar, '; ' order by reqnum) lotuuid, 
 		string_agg(requestid::varchar, '; ' order by reqnum) requestid
@@ -52,7 +54,7 @@ select  srl.org_kgntv,
 		where rn < 100		
 	group by reqnum
 	)ais on ais.reqnum = srl.REQNUM
-    group by
+group by
     srl.org_kgntv,
     eo.orgtitle,
     dok.full_name,
@@ -60,4 +62,11 @@ select  srl.org_kgntv,
     srl.REQNUM,
     srl.publishdate_reqnum,
     srl.sop_name_reqnum,
-    ais.lotuuid, ais.requestid, flag_smp;
+    ais.lotuuid, ais.requestid, flag_smp) d
+left join 
+		(select sum(erc.nmc_reqnum) nmc_reqnum, 
+				sum(erc.ck_first) ck_first, 
+				erc.reqnum 
+			from nrpz.erc_25_list_contract erc
+			where erc.flag_comp_reqnum = 1 
+			group by erc.reqnum )erc on erc.reqnum = d.reqnum;
