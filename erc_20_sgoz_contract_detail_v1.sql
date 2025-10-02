2162bd8567dd5cafa50f111ff2e0b55fedbbc8ad
Басаргина Валентина <valentinabasargina@yandex.ru>
2025-10-02 14:45:59 +0300

    Update erc_20_sgoz_contract_detail_v1.sql
    Добавлен столбец iku

--- erc_20_sgoz_contract_detail_v1.sql
+++ erc_20_sgoz_contract_detail_v1.sql
@@ -22,10 +22,11 @@ Select fin.customerinn,
        fin.pg_rn,
        fin.finsum,
        fin.finsum_f, fin.targetexpenseitemcode, fin.cvr, fin.kosgu,
-       finsum fin_now
+       finsum fin_now,
+	   org.iku
 From
       (
-         select distinct dok.id, dok.inn
+         select distinct dok.id, dok.inn, dok.iku
          from nrpz.erc_dwh_organization_kgntv dok
          join nrpz.erc_dwh_organization_kgntv dokgrbs	on dokgrbs.id = dok.parentid
          join nrpz.ERC_ORGANIZATION eo	on eo.spz = dokgrbs.spz
