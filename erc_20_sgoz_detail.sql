d34db7cf341bd0d6368bff242d1ea379920a6a90
sannikova.a <sannikova.a@IMC.SPB.RU>
2026-02-18 09:54:25 +0300

    сгоз суммируется по одинаковым цс, квр и косгу

--- erc_20_sgoz_detail.sql
+++ erc_20_sgoz_detail.sql
@@ -12,20 +12,21 @@ SELECT s.contragent_inn,
        s.sum As sgoz
 FROM( 
     -- ГБУ
-    SELECT s.contragent_inn, s.contragent_account, s.budget_type, s.targetexpenseitemcode, s.cvr::int2, s.kosgu::int2, s.sum, year
+    SELECT s.contragent_inn, s.contragent_account, s.budget_type, s.targetexpenseitemcode, s.cvr::int2, s.kosgu::int2, sum(s.sum), year
 	FROM nrpz.erc_dwh_kf_sgoz_kgntv_${srez_number} s
 	LEFT JOIN nrpz.erc_dwh_organization_kgntv o ON (s.contragent_inn=o.inn)
 	WHERE s.year=20${year}
 			AND o.role_code IN (3) --ГБУ
-			AND s.budget_type IN ('СИЦ','СГЗ')
+			AND s.budget_type IN ('СИЦ','СГЗ', 'BUD')
 			AND (s.kosgu BETWEEN '221' AND '229' OR s.kosgu BETWEEN '340' AND '349' OR s.kosgu IN ('214', '310', '320', '352', '353')
 			OR (s.kosgu='263' AND s.contragent_inn NOT IN ('7825357195')))
 	        AND o.full_name not like '%АВТОНОМНОЕ%'
+	group by   s.contragent_inn, s.contragent_account, s.budget_type, s.targetexpenseitemcode, s.cvr::int2, s.kosgu::int2, year      
     
 	UNION
     
     -- ИОГВ, ГКУ 
-    SELECT s.contragent_inn, s.contragent_account, 'BUD' budget_type, s.targetexpenseitemcode, s.cvr::int2, s.kosgu::int2, s.lbo, year
+    SELECT s.contragent_inn, s.contragent_account, 'BUD' budget_type, s.targetexpenseitemcode, s.cvr::int2, s.kosgu::int2, sum(s.sum), year
 	FROM nrpz.erc_dwh_lbo_kgntv_${srez_number} s
 	LEFT JOIN nrpz.erc_dwh_organization_kgntv o ON (s.contragent_inn=o.inn)
 	WHERE s.year=20${year}
@@ -36,6 +37,7 @@ FROM(
 				 AND s.kosgu NOT IN ('297', '298', '299', '530') --267 убрали от 01.04.25
 				 AND s.contragent_account NOT IN ('0294002','0294003','0294004','0244001','0244010','0934044', '0244031')) --добавили '0934044', '0244031' от 01.04.25
 			AND inn !='7802072429' --выключено от 18.04.24 стал ГБУ
+	group by 1,2,3,4,5,6,8
 ) s 
 JOIN 
 	(SELECT DISTINCT dok.inn, dok.full_name, dok.grbs_full_name
@@ -46,4 +48,3 @@ JOIN
 ) eo ON eo.inn=s.contragent_inn
 WHERE  s.year=20${year}
 and s.contragent_inn NOT IN ('7802139352','7803019724','7805016119','7811040938','7825666429') --12.05.23 
-;
